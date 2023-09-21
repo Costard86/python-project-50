c591c0a5c10c133d4fcefa881a16034b2943eeda
Costard86 <Costard86@gmail.com>
2023-09-21 20:44:48 +0700

    Step 3 Done

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Costard86/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/Costard86/python-project-50/actions)
+
+[![asciicast](https://asciinema.org/a/hM8RceqWshuKZowfQ5IxZZDK7.svg)](https://asciinema.org/a/hM8RceqWshuKZowfQ5IxZZDK7)
--- gendiff/fuctions/generate_diff.py
+++ gendiff/fuctions/generate_diff.py
@@ -2,25 +2,24 @@ import json
 
 
 def generate_diff(file1, file2):
-#    with open(file1, 'r') as input_file1, open(file2, 'r') as input_file2:
-        data1 = json.loads(open(file1))
-        data2 = json.loads(open(file2))
+    data1 = json.loads(open(file1).read())
+    data2 = json.loads(open(file2).read())
 
-        sorted_dict1 = dict(sorted(data1.items(), key=lambda x: x[0]))
-        sorted_dict2 = dict(sorted(data2.items(), key=lambda x: x[0]))
-        lines = ['{\n']
-        for key in sorted_dict1:
-            if key not in sorted_dict2:
-                lines.append(f"- {key} : {data1[key]}\n")
-            elif key in sorted_dict2 and sorted_dict1[key] != sorted_dict2[key]:
-                lines.append(f"- {key} : {data1[key]}\n")
-                lines.append(f"+ {key} : {data2[key]}\n")
-            elif key in sorted_dict2 and sorted_dict1[key] == sorted_dict2[key]:
-                lines.append(f"  {key} : {data1[key]}\n")
-            else:
-                lines.append('impossible\n')
-        for key in sorted_dict2:
-            if key not in sorted_dict1:
-                lines.append(f"+ {key} : {data2[key]}\n")
-        lines.append('}')
-        return " ".join(lines)
+    sorted_dict1 = dict(sorted(data1.items(), key=lambda x: x[0]))
+    sorted_dict2 = dict(sorted(data2.items(), key=lambda x: x[0]))
+    lines = ['{\n']
+    for key in sorted_dict1:
+        if key not in sorted_dict2:
+            lines.append(f"- {key} : {data1[key]}\n")
+        elif key in sorted_dict2 and sorted_dict1[key] != sorted_dict2[key]:
+            lines.append(f"- {key} : {data1[key]}\n")
+            lines.append(f"+ {key} : {data2[key]}\n")
+        elif key in sorted_dict2 and sorted_dict1[key] == sorted_dict2[key]:
+            lines.append(f"  {key} : {data1[key]}\n")
+        else:
+            lines.append('impossible\n')
+    for key in sorted_dict2:
+        if key not in sorted_dict1:
+            lines.append(f"+ {key} : {data2[key]}\n")
+    lines.append('}')
+    return " ".join(lines)
