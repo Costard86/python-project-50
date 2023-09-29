8022140c1da4f4e53a40f6ec3c95ad536f825467
Costard86 <Costard86@gmail.com>
2023-09-29 11:34:58 +0700

    gitignore + fix generate_diff

--- gendiff/fuctions/generate_diff.py
+++ gendiff/fuctions/generate_diff.py
@@ -11,8 +11,8 @@ def generate_diff(file1, file2):
 
     lines = ['{\n']
     for key in all_keys:
-        value1 = data1.get(key)
-        value2 = data2.get(key)
+        value1 = true_false_lower(data1.get(key))
+        value2 = true_false_lower(data2.get(key))
 
         if key not in keys2:
             lines.append(f" - {key}: {value1}\n")
@@ -25,3 +25,13 @@ def generate_diff(file1, file2):
             lines.append(f"   {key}: {value1}\n")
     lines.append('}')
     return "".join(lines)
+
+
+def true_false_lower(data):
+    if data is True:
+        data = 'true'
+    if data is False:
+        data = 'false'
+    if data is None:
+        data = 'null'
+    return data
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -11,9 +11,9 @@ def get_path(file_name):
 def test_merge(tmp_path):
     file1 = get_path('file1.json')
     file2 = get_path('file2.json')
-    expected = ('{\n - follow: False\n   host: hexlet.io\n '
+    expected = ('{\n - follow: false\n   host: hexlet.io\n '
                 '- proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n '
-                '+ verbose: True\n}')
+                '+ verbose: true\n}')
 
     result = generate_diff(file1, file2)
     assert result == expected
@@ -22,7 +22,7 @@ def test_merge(tmp_path):
 def test_merge_same_text(tmp_path):
     file1 = get_path('file1.json')
     file2 = get_path('file3.json')
-    expected = ('{\n   follow: False\n   host: hexlet.io\n   '
+    expected = ('{\n   follow: false\n   host: hexlet.io\n   '
                 'proxy: 123.234.53.22\n   timeout: 50\n}')
 
     result = generate_diff(file1, file2)
