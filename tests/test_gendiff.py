663df5497338bca32d7468afb292d3e053c0ef15
Costard86 <Costard86@gmail.com>
2023-12-11 18:27:10 +0700

    Autotesting fixed linter

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -12,9 +12,9 @@ def get_path(file_name):
 def test_merge(tmp_path):
     file1 = get_path('file1.json')
     file2 = get_path('file2.json')
-    expected = ('{\n  - follow: false\n    host: hexlet.io\n  '
-                '- proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  '
-                '+ verbose: true\n}')
+    expected = ('{\n  - follow: false\n    host: hexlet.io\n  - proxy: '
+                '123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + '
+                'verbose: true\n}')
 
     result = generate_diff(file1, file2, 'stylish')
     assert result == expected
@@ -23,9 +23,9 @@ def test_merge(tmp_path):
 def test_merge_yml_json(tmp_path):
     file1 = get_path('filepath1.yml')
     file2 = get_path('file2.json')
-    expected = ('{\n  - follow: false\n    host: hexlet.io\n  '
-                '- proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  '
-                '+ verbose: true\n}')
+    expected = ('{\n  - follow: false\n    host: hexlet.io\n  - proxy: '
+                '123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + '
+                'verbose: true\n}')
 
     result = generate_diff(file1, file2, 'stylish')
     assert result == expected
